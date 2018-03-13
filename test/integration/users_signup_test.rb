d607b69714076f59461ba563d1e9e7ce22171e44
David Michaux <michaux.dr@gmail.com>
2018-03-13 14:35:19 -0400

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 																				 password_confirmation: "validpass" } }
 		end
 		follow_redirect!
+		assert is_logged_in?
 		assert_template "users/show"
 		assert_not flash.empty?
 	end
